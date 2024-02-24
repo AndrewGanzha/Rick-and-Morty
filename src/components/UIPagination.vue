71ba33021918c15bf11cdf8076c5a66e04ce428e
AndrewGanzha <ganzha-2002@mail.com>
2024-02-24 15:11:53 +0300

    Relase new logic for load pagination

--- src/components/UIPagination.vue
+++ src/components/UIPagination.vue
@@ -1,17 +1,35 @@
 <script setup lang="ts">
-defineProps({
+import {computed} from "vue";
+
+const props = defineProps({
   currentPage: Number,
   totalPages: Number
 })
 
 const emits = defineEmits(['next-pages'])
+const nearbyPages = computed(() => {
+  let nearby = [];
+
+  if(props.currentPage && props.totalPages) {
+    for (let i = props.currentPage - 3; i <= props.currentPage + 3; i++) {
+      if (i > 0 && i <= props.totalPages && i < props.totalPages && i !== 1) {
+        nearby.push(i);
+      }
+    }
+    nearby = [1, ...nearby, props.totalPages]
+    return nearby;
+  } else {
+    return null;
+  }
+
+})
 </script>
 
 <template>
   <ul class="pagination">
     <li>
       <button
-        v-for="pageNumber in totalPages"
+        v-for="pageNumber in nearbyPages"
         :class="{ 'active-page': pageNumber === currentPage }"
         :key="pageNumber"
         @click="emits('next-pages', pageNumber)"
